1ca1a6191699b2ad8489d44f5777daa14ead58b5
JerryD <jerry.webdev@gmail.com>
2022-02-10 17:30:44 -0500

    Imports the node module "path" within the webpack config file. Adds the "entry" object within the webpack config file. Defines the "index" property within the "entry" object. Defines the "path" property within the "entry" object.

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, "src", "index.js"),
+        path: path.resolve(__dirname, "build")
+    }
+}
