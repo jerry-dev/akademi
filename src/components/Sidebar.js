b40b378c741b0f35209c9496eb73f007582bcc27
JerryD <jerry.webdev@gmail.com>
2022-02-11 14:31:16 -0500

    Adds “sidebar.module.css” within “src/components” Defines a “aside” class within “sidebar.module.css” and provides it the following CSS rules: 	background-color: var(--color-1) 	max-width: 21.5625rem; Imports “src/components/sidebar.module.css” within “src/components/Sidebar.js” as “styles” and adds styles.aside to the aside element’s className JSX attribute.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import NavList from './NavList.js';
+import styles from './sidebar.module.css';
 
 const Sidebar = () => {
     return (
-        <aside>
+        <aside className={styles.aside}>
             {/* <CompanyLogo/> */}
             <NavList/>
         </aside>
