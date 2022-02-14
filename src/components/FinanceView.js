a9b11645e75f5ebc0fe38e03607e5362bfd3cac7
JerryD <jerry.webdev@gmail.com>
2022-02-14 17:45:07 -0500

    Creates the FinanceView.js component and financeview.module.css
    FinanceView.js is the view for the “Finance” section of the dashboard.
    Will act as the container component for various presentational components shown in the design.

---
+++ src/components/FinanceView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './financeview.module.css';
+
+const FinanceView = () => {
+    return (
+        <main>
+            <div>
+                <h1>FINANCE VIEW: WORK IN PROGRESS</h1>
+            </div>
+        </main>
+    );
+}
+
+export default FinanceView;
