f61dde00efb58a9ad48c4b3c5f5617fd644742ed
JerryD <jerry.webdev@gmail.com>
2022-02-14 17:58:20 -0500

    Creates the ChatView.js component and chatview.module.css
    ChatView.js is the view for the “Chat” section of the dashboard.
    Will act as the container component for various presentational components shown in the design.

---
+++ src/components/ChatView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './chatview.module.css';
+
+const ChatView = () => {
+    return (
+        <main>
+            <div>
+                <h1>CHAT VIEW: WORK IN PROGRESS</h1>
+            </div>
+        </main>
+    );
+}
+
+export default ChatView;
