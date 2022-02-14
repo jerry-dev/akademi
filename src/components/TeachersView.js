c37e013f6c53a16b0dd7558408c26cd0feab494a
JerryD <jerry.webdev@gmail.com>
2022-02-14 17:33:56 -0500

    Creates the TeachersView.js component and teachersview.module.css
    TeachersView.js is the view for the “Teachers” section of the dashboard.
    Will act as the container component for various presentational components shown in the design.

---
+++ src/components/TeachersView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './teachersview.module.css';
+
+const TeachersView = () => {
+    return (
+        <main>
+            <div>
+                <h1>TEACHERS VIEW: WORK IN PROGRESS</h1>
+            </div>
+        </main>
+    );
+}
+
+export default TeachersView;
