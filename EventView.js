6d597876bb3ec770312c145a6927d8ddeb57b1e1
JerryD <jerry.webdev@gmail.com>
2022-02-14 17:38:45 -0500

    Creates the EventView.js component and eventview.module.css
    EventView.js is the view for the “Event” section of the dashboard.
    Will act as the container component for various presentational components shown in the design.

---
+++ EventView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './eventview.module.css';
+
+const EventView = () => {
+    return (
+        <main>
+            <div>
+                <h1>EVENT VIEW: WORK IN PROGRESS</h1>
+            </div>
+        </main>
+    );
+}
+
+export default EventView;
