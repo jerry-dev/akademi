70c2ffb7e8a6c35a2a5a0ecd3dab88cafbd6806e
JerryD <jerry.webdev@gmail.com>
2022-02-14 18:01:29 -0500

    Creates the ActivityView.js component and activityview.module.css
    ActivityView.js is the view for the “Latest Activity” section of the dashboard.
    Will act as the container component for various presentational components shown in the design.

---
+++ src/components/ActivityView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './activityview.module.css';
+
+const ActivityView = () => {
+    return (
+        <main>
+            <div>
+                <h1>LATEST ACTIVITY VIEW: WORK IN PROGRESS</h1>
+            </div>
+        </main>
+    );
+}
+
+export default ActivityView;
