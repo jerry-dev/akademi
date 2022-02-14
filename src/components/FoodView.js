e2fe5e4e49b267545d708ae24a5860a2cef8574a
JerryD <jerry.webdev@gmail.com>
2022-02-14 17:48:21 -0500

    Creates the FoodView.js component and foodview.module.css
    FoodView.js is the view for the “Food” section of the dashboard.
    Will act as the container component for various presentational components shown in the design.

---
+++ src/components/FoodView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './foodview.module.css';
+
+const FoodView = () => {
+    return (
+        <main>
+            <div>
+                <h1>FOOD VIEW: WORK IN PROGRESS</h1>
+            </div>
+        </main>
+    );
+}
+
+export default FoodView;
