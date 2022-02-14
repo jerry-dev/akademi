a006e2ae7fb8d351f9024634aabae4e7276c0593
JerryD <jerry.webdev@gmail.com>
2022-02-14 17:30:23 -0500

    Creates the StudentsView.js component and studentsview.module.css
    StudentsView.js is the view for the “Students” section of the dashboard.
    Will act as the container component for various presentational components shown in the design.

---
+++ src/components/StudentsView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './studentsview.module.css';
+
+const StudentsView = () => {
+    return (
+        <main>
+            <div>
+                <h1>STUDENTS VIEW: WORK IN PROGRESS</h1>
+            </div>
+        </main>
+    );
+}
+
+export default StudentsView;
