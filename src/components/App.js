78c1604357136395f478782f2879f7ff907b409b
JerryD <jerry.webdev@gmail.com>
2022-02-14 17:12:51 -0500

    Creates the HomeView.js component and homeview.module.css
    HomeView.js is the view for the “Dashboard” section of the dashboard.
    Will act as the container component for various presentational components shown in the design.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Sidebar from './Sidebar.js';
+import HomeView from './HomeView.js';
 
 const App = () => {
     return (
         <React.Fragment>
             <Sidebar/>
-            {/* <Board/> */}
+            <HomeView/>
         </React.Fragment>
     )
 }
---
+++ src/components/HomeView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './homeview.module.css';
+
+const HomeView = () => {
+    return (
+        <main>
+            <div className={styles.homeViewMainInnerContainer}>
+                <h1>TEST</h1>
+            </div>
+        </main>
+    );
+}
+
+export default HomeView;
