103b20b2cb903ebbf4eba1153e1ff1ca52f0e86f
JerryD <jerry.webdev@gmail.com>
2022-02-14 17:54:15 -0500

    Creates the UserView.js component and userview.module.css
    UserView.js is the view for the “User” section of the dashboard.
    Will act as the container component for various presentational components shown in the design.

---
+++ src/components/UserView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './userview.module.css';
+
+const UserView = () => {
+    return (
+        <main>
+            <div>
+                <h1>USER VIEW: WORK IN PROGRESS</h1>
+            </div>
+        </main>
+    );
+}
+
+export default UserView;
