0cfa900938ccb86b00691f980c70cebd7a165d8a
JerryD <jerry.webdev@gmail.com>
2022-02-15 13:01:11 -0500

    Adds the NavElement highlighting
    Defined the JavaScript logic and CSS logic to highlight the active
    view.

--- src/components/NavElement.js
+++ src/components/NavElement.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import styles from './navelement.module.css';
 import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 const NavElement = (props) => {
     const altValue = `${props.text} route icon`;
+
+    let location = useLocation();
+    let isActive = {'data-isactive': "false"};
+    let thePath = (props.path === '/') ? '/' : `/${props.path}`;
+
+    (location.pathname === `${thePath}`)
+            ? isActive = {'data-isactive': "true"}
+            : isActive = {'data-isactive': "false"};
+
+    React.useEffect(() => {       
+        (location.pathname === `${thePath}`)
+            ? isActive = {'data-isactive': "true"}
+            : isActive = {'data-isactive': "false"};
+    }, [location]);
     
     return (
-        <Link className={styles.navElement} to={props.path}>
+        <Link className={styles.navElement} {...isActive} to={props.path}>
             <span>
                 <img src={props.icon} alt={altValue}/>
                 {props.text}
