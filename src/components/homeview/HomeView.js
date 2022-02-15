c8c7c2383ca11daa7e2e2866122feb90a0a1c69c
JerryD <jerry.webdev@gmail.com>
2022-02-15 16:09:36 -0500

    Adds The SectionTilte Component
    Create the sectiontitle directory within “src/components”, and within it creates
    SectionTitle.js. A sectionTitle class was defined within the global css file (src/index.css).
    Within the HomeView.js module, we import and utilize said SectionTitle component.

--- src/components/homeview/HomeView.js
+++ src/components/homeview/HomeView.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './homeview.module.css';
+import SectionTitle from '../sectiontitle/SectionTitle.js';
 
 const HomeView = () => {
     return (
         <main>
             <div className={styles.homeViewMainInnerContainer}>
-                <h1>HOME VIEW: WORK IN PROGRESS</h1>
+                <SectionTitle title="Dashboard"/>
             </div>
         </main>
     );
