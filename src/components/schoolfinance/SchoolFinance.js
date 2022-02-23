4d50f8bfa5cdcbb726fdd1634fae06cef8e32346
JerryD <jerry.webdev@gmail.com>
2022-02-23 15:51:32 -0500

    Defines The SchoolFinance Component Header

--- src/components/schoolfinance/SchoolFinance.js
+++ src/components/schoolfinance/SchoolFinance.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import styles from './schoolfinance.module.css';
+import SectionTitle from '../sectiontitle/SectionTitle.js';
+import ChartMetric from '../chartmetric/ChartMetric.js';
 
 const SchoolFinance =  (props) => {
     return (
-        <section className={styles.schoolFinance}></section>
+        <section className={styles.schoolFinance}>
+            <div>
+                <header>
+                    <SectionTitle title="School Finance"/>
+                    <div>
+                        <ChartMetric title="This Week" stat="1.245" style="one"/>
+                        <ChartMetric title="Last Week" stat="1.356" style="two"/>
+                    </div>
+                </header>
+            </div>
+        </section>
     );
 }
 
