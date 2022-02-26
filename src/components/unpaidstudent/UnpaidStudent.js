ec6ef4bb34571b2db35d76de045deb6af99b15a1
JerryD <jerry.webdev@gmail.com>
2022-02-26 13:08:19 -0500

    Adds A Title To The Unpaid Student Intuition Component
    Imports the SectionTitle component within the UnpaidStudent.js module, and invokes
    it within the header.

--- src/components/unpaidstudent/UnpaidStudent.js
+++ src/components/unpaidstudent/UnpaidStudent.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import styles from './unpaidstudent.module.css';
+import SectionTitle from '../sectiontitle/SectionTitle.js';
 
 const UnpaidStudent =  (props) => {
     return (
-        <section className={styles.unpaidStudent}></section>
+        <section className={styles.unpaidStudent}>
+            <div>
+                <header>
+                    <SectionTitle title="Unpaid Student Intuition"/>
+                    <div>
+                    </div>
+                </header>
+            </div>
+        </section>
     );
 }
 
