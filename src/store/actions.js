cceab152d2ef5a884c214dc05670bb7089ca67d7
JerryD <jerry.webdev@gmail.com>
2022-03-02 16:20:30 -0500

    Add A Method Called “getHomeData” To The Exported Object Within src/store/actions.js
    This method creates an empty object called “homeData”, then invokes the store.getState()
    method to append both the “overview” property and “unpaidstudentintuition” property to the
    “homeData”. The “homeData” is then returned.

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,2 +1,8 @@
 export default {
+    getHomeData() {
+        const homeData = {};
+        homeData.overview = store.getState().overview;
+        homeData.unpaidstudentintuition = store.getState().unpaidstudentintuition;
+        return homeData;
+    },
 }
