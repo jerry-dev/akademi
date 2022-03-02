ccef5a6f210d22a780b286d61fc00b705e260e3d
JerryD <jerry.webdev@gmail.com>
2022-03-02 17:06:23 -0500

    Add Moved The Importing of Both The “store” Object and “Provider” Element From src/components/App.js To src/index.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,8 +10,6 @@ import UserView from '../userview/UserView.js';
 import ChatView from '../chatview/ChatView.js';
 import ActivityView from '../activityview/ActivityView.js';
 import { Routes, Route } from 'react-router-dom';
-import store from '../../store/index.js';
-import { Provider } from 'react-redux';
 
 const App = () => {
     return (
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import App from './components/app/App.js';
 import { HashRouter } from "react-router-dom";
+import store from './store/index.js';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
     <HashRouter>
