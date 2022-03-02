960f54b66b9c6dae569708aa3eef1d4e3de665b8
JerryD <jerry.webdev@gmail.com>
2022-03-02 14:59:52 -0500

    Add “overview” Property With Data Within The initialState Object Within src/store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,10 @@
 import {createStore } from 'redux';
 
 const initialState = {
+    overview: [
+        { "title": "students", "stat": "932" },
+        { "title": "teacher", "stat": "754" },
+        { "title": "events", "stat": "40" },
+        { "title": "foods", "stat": "32,000" },
+    ]
 }
