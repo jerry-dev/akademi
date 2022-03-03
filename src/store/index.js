837ed63aa894ca78157feef9de27fc0dd0642ca9
JerryD <jerry.webdev@gmail.com>
2022-03-03 08:11:57 -0500

    Add Pluralizing The “teacher” Value To The “title” of The Second Object in The initialState Object of The overview Property Within src/store/index.js
    This change was made so that the name of the import teachers icon can match the title
    In Overview.js.

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import actions from './actions.js';
 const initialState = {
     overview: [
         { "title": "students", "stat": "932" },
-        { "title": "teacher", "stat": "754" },
+        { "title": "teachers", "stat": "754" },
         { "title": "events", "stat": "40" },
         { "title": "foods", "stat": "32,000" },
     ],
