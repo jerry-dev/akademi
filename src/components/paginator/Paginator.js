c39b934ded5f6b645abf644b8c16aa54dbbf79f9
JerryD <jerry.webdev@gmail.com>
2022-03-04 09:44:05 -0500

    Add The “onClick={props.buttonsHandler}” Invocation On Each of The Paginator Component’s Buttons WIthin Paginator.js

--- src/components/paginator/Paginator.js
+++ src/components/paginator/Paginator.js
@@ -6,21 +6,21 @@ import Next from '../../assets/icons/shared/next.svg';
 const Paginator = (props) => {
     return (
         <nav className={styles.paginator}>
-            <button type="button" id="previousArrow" className={styles.arrow}>
+            <button type="button" id="previousArrow" className={styles.arrow} onClick={props.buttonsHandler}>
                 <img src={Previous} alt="Left arrow"/>
             </button>
             <div>
-                <button data-selected="true" type="button" id="firstCircleNav" className={styles.circleNav}>
+                <button data-selected="true" type="button" id="firstCircleNav" className={styles.circleNav} onClick={props.buttonsHandler}>
                     <output>1</output>
                 </button>
-                <button type="button" id="secondCircleNav" className={styles.circleNav}>
+                <button type="button" id="secondCircleNav" className={styles.circleNav} onClick={props.buttonsHandler}>
                     <output>2</output>
                 </button>
-                <button type="button" id="thirdCircleNav" className={styles.circleNav}>
+                <button type="button" id="thirdCircleNav" className={styles.circleNav} onClick={props.buttonsHandler}>
                     <output>3</output>
                 </button>
             </div>
-            <button type="button" id="nextArrow" className={styles.arrow}>
+            <button type="button" id="nextArrow" className={styles.arrow} onClick={props.buttonsHandler}>
                 <img src={Next} alt="Right arrow"/>
             </button>
         </nav>
