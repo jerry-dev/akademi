c0fbbb3c85f8433cd5b1e569ed0e94999a913f37
JerryD <jerry.webdev@gmail.com>
2022-03-04 10:13:49 -0500

    Add The Repositioning Of The “onClick={props.buttonsHandler}” Invocation From The Buttons On The Paginator Components To The nav Element Container WIthin Paginator.js
    Moving the handler to the container will facility for more powerful event delegation based
    operations, while making the markup cleaner.

--- src/components/paginator/Paginator.js
+++ src/components/paginator/Paginator.js
@@ -5,22 +5,22 @@ import Next from '../../assets/icons/shared/next.svg';
 
 const Paginator = (props) => {
     return (
-        <nav className={styles.paginator}>
-            <button type="button" id="previousArrow" className={styles.arrow} onClick={props.buttonsHandler}>
+        <nav className={styles.paginator} onClick={props.buttonsHandler}>
+            <button type="button" id="previousArrow" className={styles.arrow}>
                 <img src={Previous} alt="Left arrow"/>
             </button>
             <div>
-                <button data-selected="true" type="button" id="firstCircleNav" className={styles.circleNav} onClick={props.buttonsHandler}>
+                <button data-selected="true" type="button" id="firstCircleNav" className={styles.circleNav}>
                     <output>1</output>
                 </button>
-                <button type="button" id="secondCircleNav" className={styles.circleNav} onClick={props.buttonsHandler}>
+                <button type="button" id="secondCircleNav" className={styles.circleNav}>
                     <output>2</output>
                 </button>
-                <button type="button" id="thirdCircleNav" className={styles.circleNav} onClick={props.buttonsHandler}>
+                <button type="button" id="thirdCircleNav" className={styles.circleNav}>
                     <output>3</output>
                 </button>
             </div>
-            <button type="button" id="nextArrow" className={styles.arrow} onClick={props.buttonsHandler}>
+            <button type="button" id="nextArrow" className={styles.arrow}>
                 <img src={Next} alt="Right arrow"/>
             </button>
         </nav>
