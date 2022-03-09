9add912bbc4df7c1a3d0b998928172d95027b67c
JerryD <jerry.webdev@gmail.com>
2022-03-09 18:17:57 -0500

    LOGIC REFACTOR: Use Image To Display Message Sender’s Name Within MessageListElement.js
    Problem
    The element wasn’t utilizing the photo source to display a photo, nor was there an value for
    the alt attribute.
    
    Solution
    - Used props.profilePhoto For src attribute’s value
    - Created a string concatenated wIth props.sendername for the alt attribute’s value

--- src/components/messagelistelement/MessageListElement.js
+++ src/components/messagelistelement/MessageListElement.js
@@ -2,10 +2,12 @@ import React from 'react';
 import styles from './messagelistelement.module.css';
 
 const MessageListElement = (props) => {
+    const altValue = `Student ${props.senderName}'s profile photo`;
+
     return (
         <article className={styles.messageListElement}>
             <figure>
-                <img/>
+                <img src={props.profilePhoto} alt={altValue}/>
                 <figcaption>
                     <h6>{props.senderName}</h6>
                     <p>{props.messagePreview}</p>
