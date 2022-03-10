0d4971897885100826611e06965962371c5fecbe
JerryD <jerry.webdev@gmail.com>
2022-03-10 16:49:07 -0500

    LOGIC REFACTOR: Add Image Handling Logic To The CurrentFoodsItem Component Within CurrentFoodsItem.js
    Problem
    The CurrentFoodsitem component isn’t displaying its associated image.
    
    Solution
    - Imported the images that will be used for the food items being displayed in “Current Foods Menu”
    - Created a case statement to handle which image to display based off of the props.itemid
    - Added a src attribute to the img tag, and provided it a container that features the associated image

--- src/components/currentfoodsitem/CurrentFoodsItem.js
+++ src/components/currentfoodsitem/CurrentFoodsItem.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './currentfoodsitem.module.css';
+import burger from '../../assets/images/menuItems/burger.jpg';
+import smoothie from '../../assets/images/menuItems/smoothie.jpg';
+import beefsteak from '../../assets/images/menuItems/beefsteak.jpg';
+import pancakes from '../../assets/images/menuItems/pancakes.jpg';
+import pizza from '../../assets/images/menuItems/pizza.jpg';
+import shrimpsalad from '../../assets/images/menuItems/shrimpsalad.jpg';
+import raman from '../../assets/images/menuItems/raman.jpg';
+
 
 const CurrentFoodsItem = (props) => {
+    let image = {};
+
+    switch (props.itemId) {
+        case 1: image = beefsteak; break;
+        case 2: image = pancakes; break;
+        case 3: image = raman; break;
+        case 4: image = burger; break;
+        case 5: image = pizza; break;
+        case 6: image = shrimpsalad; break;
+        case 7: image = smoothie; break;
+    }
+
     const altValue = `Image of the ${props.menuItemName} menu item.`;
     return (
         <article className={styles.currentFoodItem}>
             <figure>
-                <img alt={altValue}/>
+                <img src={image} alt={altValue}/>
                 <figcaption>
                     <h5>{props.menuItemName}</h5>
                     <p>{props.menuItemPreviewDetail}</p>
