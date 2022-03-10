5b50dd7e038a843cb41f88be6d13b69180c5e3fc
JerryD <jerry.webdev@gmail.com>
2022-03-10 15:31:06 -0500

    LOGIC REFACTOR: Add The Processing, Displaying, And Handling of The Menu Items Data Within CurrentFoodsMenu.js
    Problem
    The CurrentFoodsMenu component is working with hardcoded data, and its view more button
    is not functioning.
    
    Solution
    - Added data bucketing to CurrentFooodsMenu’s props.currentFoodsItems data
    - Programmed a viewMore function to handle the view more button

--- src/components/currentfoodsmenu/CurrentFoodsMenu.js
+++ src/components/currentfoodsmenu/CurrentFoodsMenu.js
@@ -3,26 +3,61 @@ import styles from './currentfoodsmenu.module.css';
 import SectionTitle from '../sectiontitle/SectionTitle.js';
 import CurrentFoodsItem from '../currentfoodsitem/CurrentFoodsItem.js';
 
-const menuItems = [
-    <li>
-        <CurrentFoodsItem menuItemName="Beef Steak with Fried Potato" menuItemPreviewDetail="Lorem ipsum dolor sit amet..."/>
-    </li>,
-    <li>
-        <CurrentFoodsItem menuItemName="Pancake with Honey" menuItemPreviewDetail="Lorem ipsum dolor sit amet..."/>
-    </li>,
-    <li>
-        <CurrentFoodsItem menuItemName="Japanese Beef Ramen" menuItemPreviewDetail="Lorem ipsum dolor sit amet..."/>
-    </li>
-];
-
 const CurrentFoodsMenu = (props) => {
+    let bucketsContainer = {};
+    const maxDocsPerBucket = 3;
+    let index = 0;
+    const [bucketsBeingShown, setBucketsBeingShown] = React.useState([1]);
+
+    for (let i = 1; i < props.currentFoodsItems.length; i++) {
+        for (let k = 0; k < maxDocsPerBucket; k++) {
+            if (props.currentFoodsItems[index]) {
+                if (!Array.isArray(bucketsContainer[i])) {
+                    bucketsContainer[i] = [];
+                }
+                bucketsContainer[i].push(props.currentFoodsItems[index]);
+                index++;
+            }
+        }
+    }
+
+    const bucketsToDisplayed = [];
+    for (let i = 1; i <= Object.keys(bucketsContainer).length; i++) {
+        if (bucketsBeingShown.includes(i)) {
+            for (let j = 0; j < bucketsContainer[i].length; j++) {
+                bucketsToDisplayed[bucketsToDisplayed.length] = bucketsContainer[i][j];
+            }
+        }
+    }
+
+    const menuItems = bucketsToDisplayed.map((instance) => {
+        return <li>
+            <CurrentFoodsItem
+                menuImage={instance.menuImage}
+                menuItemName={instance.menuItemName}
+                menuItemPreviewDetail={instance.menuItemPreviewDetail}/>
+            </li>
+    });
+
+    const viewMore = () => {
+        let bucketsToShow = JSON.parse(JSON.stringify(bucketsBeingShown));
+        const nextBucket = bucketsBeingShown[bucketsBeingShown.length-1] + 1;
+
+        if (bucketsContainer[nextBucket]) {
+            bucketsToShow[bucketsToShow.length] = nextBucket;
+            setBucketsBeingShown(bucketsToShow);
+        } else {
+            console.error(`**There are no more menu items to show**`);
+        }
+    }
+
     return (
         <article className={styles.currentFoodsMenu}>
             <header>
                 <SectionTitle title="Current Foods Menu"/>
             </header>
             <ul>{menuItems}</ul>
-            <button type="button" className={styles.loadMore}>
+            <button type="button" className={styles.loadMore} onClick={viewMore}>
                 <p>View More</p>
             </button>
         </article>
