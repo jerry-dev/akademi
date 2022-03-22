74d984fcf98bd9aa3e365a10489b311e3db37943
JerryD <jerry.webdev@gmail.com>
2022-03-21 20:04:45 -0400

    REFACTOR: Add Utilization Of Logged In User Profile Photo Within LoggedInUser.js

--- src/components/loggedinuser/LoggedInUser.js
+++ src/components/loggedinuser/LoggedInUser.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './loggedinuser.module.css';
+import admin from '../../assets/images/employees/admin.jpg'
 
 const LoggedInUser = (props) => {
     return (
         <figure className={styles.loggedInUser}>
-            <img/>
+            <img src={admin} alt="Image of logged in user"/>
             <figcaption>
                 <h6>{props.userName}</h6>
                 <small>{props.role}</small>
