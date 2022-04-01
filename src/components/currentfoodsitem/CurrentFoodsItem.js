0b873dbca085e56b51c2da0a8b89b3a4fbb43398
JerryD <jerry.webdev@gmail.com>
2022-04-01 16:52:36 -0400

    REFACTOR: Removed The Images From The assets Directory That Is Now Being Serviced Via CDN, And Their References

--- src/components/currentfoodsitem/CurrentFoodsItem.js
+++ src/components/currentfoodsitem/CurrentFoodsItem.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import styles from './currentfoodsitem.module.css';
-import burger from '../../assets/images/menuItems/burger.jpg';
-import smoothie from '../../assets/images/menuItems/smoothie.jpg';
 const CurrentFoodsItem = (props) => {
-    console.log('props:', props);
-    let image = {};
-
-    switch (props.itemId) {
-        case 1: image = beefsteak; break;
-        case 2: image = pancakes; break;
-        case 3: image = raman; break;
-        case 4: image = burger; break;
-        case 5: image = pizza; break;
-        case 6: image = shrimpsalad; break;
-        case 7: image = smoothie; break;
-    }
-
     const altValue = `Image of the ${props.menuItemName} menu item.`;
     return (
         <article className={styles.currentFoodItem}>
